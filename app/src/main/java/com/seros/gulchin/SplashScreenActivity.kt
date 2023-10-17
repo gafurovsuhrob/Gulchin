c3003d5742500a153fe1fb7ebbc488792816891a
Suhrob Gafurov <98651376+gafurovsuhrob@users.noreply.github.com>
2023-10-17 22:25:24 +0500

    Initial commit

---
+++ app/src/main/java/com/seros/gulchin/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.seros.gulchin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.seros.gulchin.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        },1500)
+    }
+}
